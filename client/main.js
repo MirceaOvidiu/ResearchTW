d8d13cacc687661a4736e4279ed3d3799d6eee22
Rares-Hampi <hampimarian1323@gmail.com>
2023-12-03 20:32:52 +0200

    Add getResearchPapers function and update server response

--- client/main.js
+++ client/main.js
@@ -1,2 +1,7 @@
+const getResearchPapers = async () => {
+  const res = await axios.get("http://localhost:5000/");
+  console.log(res.data);
+  return res.data;
+};
 
-
+getResearchPapers();
--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, url_for, Response
-
-import emag_scraper
+from flask import Flask, render_template, request, url_for, redirect, Response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Add CORS to the Flask app
+
 
 
 @app.route("/")
 def index():
     return Response("Hello World!")
+    return Response("Hello World!")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
